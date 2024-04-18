13bb9af9be82a6df81cb8a98c4f4589c1cbdef54
sdy329 <sdy329@gmail.com>
2024-04-18 11:00:52 -0400

    Fixing Deployment

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,41 +1,10 @@
 import projectArray from "./ProjectArray"
-import { useEffect, useState } from "react";
-import { load } from "../routes/page.server";
 
 export default function Projects() {
-    const [userData, setUserData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const projectsArray = projectArray();
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const { users } = await load();
-                setUserData(users);
-                setLoading(false);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        }
-
-        fetchData();
-    }, []);
-
-    if (loading) {
-        return <p>Loading...</p>;
-    }
 
     return (
         <section id="projects" className="lg:mb-20 mb-10">
-            <ul>
-                {userData.map((user) => (
-                    <li key={user.id}>
-                        <p>{user.name}</p>
-                        <p>{user.email}</p>
-                        <img src={user.image} alt={user.name} />
-                    </li>
-                ))}
-            </ul>
             <div className="sticky top-0 z-20 -mx-6 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:mb-4">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:text-xl">Projects</h2>
             </div>
